0a0bfd11e137d4534a383e927aa7b292fdb80e2c
mcr45 <depr3604@protonmail.com>
2024-03-11 19:41:54 -0500

    created posts page for ALL the post not just the latest

---
+++ src/app/main/postlist/post-all/post-all.component.ts
@@ -0,0 +1,28 @@
+import { Component, OnInit } from '@angular/core';
+import { PostComponent } from '../post/post.component';
+import { Post } from '../../../shared/models/post';
+import { PostService } from '../../../core/services/post.service';
+
+@Component({
+  selector: 'app-post-all',
+  standalone: true,
+  imports: [PostComponent],
+  templateUrl: './post-all.component.html',
+  styleUrl: './post-all.component.scss'
+})
+export class PostAllComponent implements OnInit {
+  posts:Post[]=[]
+  error:string=''
+
+  constructor(private postserv:PostService){}
+ngOnInit(): void {
+    this.postserv.getPosts().subscribe(
+     { next:(res)=>{this.posts=res,console.log(this.posts)},
+      error:(err)=>{console.log(err)
+      this.error=err.message
+      }}
+    )
+}
+
+
+}
