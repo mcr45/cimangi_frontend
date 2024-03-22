d83571d337cfcbb068fb98d439a3146b0abebeab
mcr45 <depr3604@protonmail.com>
2024-03-21 19:11:51 -0500

    starting change for deploy

--- src/environments/environment.ts
+++ src/environments/environment.ts
@@ -1,6 +1,6 @@
 export const environment = {
   production:true,
-  apiUrl:'https://Cucinadipina.com',
-  logInUrl:'http://localhost:3000/login',
-  commentPostUrl:'http://Cucinadipina/posts/'
+  apiUrl:'https://cimangi.onrender.com',
+  logInUrl:'https://cimangi.onrender.com/login',
+  commentPostUrl:'https://cimangi.onrender.com/'
 };
