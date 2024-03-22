d00498446f6343633a5d7057f193825755b12fcf
mcr45 <depr3604@protonmail.com>
2024-03-21 19:17:28 -0500

    change address

--- src/app/core/services/authentication.service.ts
+++ src/app/core/services/authentication.service.ts
@@ -2,7 +2,7 @@ import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Router } from '@angular/router';
 import { BehaviorSubject } from 'rxjs';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 
 @Injectable({
   providedIn: 'root',
--- src/app/core/services/post.service.ts
+++ src/app/core/services/post.service.ts
@@ -1,6 +1,6 @@
 import { HttpClient } from '@angular/common/http';
 import { Injectable } from '@angular/core';
-import { environment } from '../../../environments/environment.development';
+import { environment } from '../../../environments/environment';
 import { Post } from '../../shared/models/post';
 import { Observable } from 'rxjs';
 
