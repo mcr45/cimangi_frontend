811a7222d060750486c76fabe352aebc275c60cd
mcr45 <depr3604@protonmail.com>
2024-03-26 21:06:22 -0500

    still working on recipe form,too many litte bugs keeping me up

--- src/app/main/recipelist/recipe-form/recipe-form.component.ts
+++ src/app/main/recipelist/recipe-form/recipe-form.component.ts
@@ -1,12 +1,23 @@
 import { Component } from '@angular/core';
-
+import { FormsModule } from '@angular/forms';
+import { NgForm } from '@angular/forms';
+import { RecipeService } from '../../../core/services/recipe.service';
 @Component({
   selector: 'app-recipe-form',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './recipe-form.component.html',
   styleUrl: './recipe-form.component.scss'
 })
 export class RecipeFormComponent {
+
+constructor(private rs:RecipeService){}
+
+onCreateForm(form:NgForm){
+
+  console.log(form)
+
+}
+
 
 }
