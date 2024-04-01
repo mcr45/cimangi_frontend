5c7398c680b29ec4798e0e3994c3e8b815ab8d85
mcr45 <depr3604@protonmail.com>
2024-03-31 23:09:40 -0500

    created new branch for possible style on homepage

---
+++ src/app/main/timeline/forum/forum.component.ts
@@ -0,0 +1,27 @@
+import { Component, OnInit } from '@angular/core';
+import { PostComponent } from '../../postlist/post/post.component';
+import { RecipeComponent } from '../../recipelist/recipe/recipe.component';
+import { Recipe } from '../../../shared/models/recipe';
+import { RecipeService } from '../../../core/services/recipe.service';
+@Component({
+  selector: 'app-forum',
+  standalone: true,
+  imports: [RecipeComponent],
+  templateUrl: './forum.component.html',
+  styleUrl: './forum.component.scss'
+})
+export class ForumComponent implements OnInit{
+
+recipes!:Recipe[]
+recipe!:Recipe
+
+constructor(private rs:RecipeService){}
+
+ngOnInit(): void {
+  this.rs.getRecipes().subscribe(({next:(res:any)=>{this.recipes=res},
+  error:(err)=>{console.log(err)}}))
+  this.recipe=this.recipes[this.recipes.length-1]
+}
+
+
+}
